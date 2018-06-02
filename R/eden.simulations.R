6a0b7fb68bf0e8b888dedbf83cf3cccd3cfa02d3
Gabriele Baldassarre <gabriele@gabrielebaldassarre.com>
2018-06-03 00:56:10 +0200

    first run

---
+++ R/eden.simulations.R
@@ -0,0 +1,83 @@
+######################
+# REQUIREMENTS       #
+######################
+if (!require("pacman")) install.packages("pacman"); invisible(library(pacman))
+tryCatch({
+  p_load("MonteCarlo", "dplyr", "futile.logger", "ggplot2", "scales", "snow", "purrrlyr")
+}, warning=function(w){
+  stop(conditionMessage(w))
+})
+
+########
+# Setup
+#
+sapply(file.path("R", c("genesi.R")),
+       source,
+       .GlobalEnv)
+
+flog.threshold(INFO)
+
+#flog.appender(appender.file('eden.sim.log'))
+flog.appender(appender.console())
+
+# Example without parallization
+veil_grid <- c("asiyah", "yetzirah", "aziluth")
+pillar_grid <- c("strong", "medium", "weak")
+malkuth_grid <- c(0)
+daat_grid <- 0:7
+dice_grid <- c(6, 8, 10, 12)
+
+param_list = list("veil" = veil_grid,
+                "pillar" = pillar_grid,
+                "malkuth" = malkuth_grid,
+                "daat" = daat_grid,
+                "dice.size" = dice_grid)
+
+# param_list = list("veil" = c("asiyah"),
+#                 "pillar" = pillar_grid,
+#                 "malkuth" = 0,
+#                 "daat" = 0:1,
+#                 "dice.size" = 8)
+
+eden.erg <- MonteCarlo(func = genesi,
+                  nrep = 100,
+                  param_list = param_list,
+                  ncpus = 1)
+
+
+eden.sim <- MakeFrame(output = eden.erg)
+
+eden.sim$pillar <- gsub("pillar=", "", eden.sim$pillar)
+eden.sim$veil <- gsub("veil=", "", eden.sim$veil)
+
+eden.sim$total.success <- eden.sim$revelation + eden.sim$sefiroth + eden.sim$success
+eden.sim$atleast.success <- (eden.sim$revelation > 0) | (eden.sim$sefiroth > 0) | (eden.sim$success > 0)
+
+eden.sim$veil <- factor(eden.sim$veil, levels = c("aziluth", "yetzirah", "asiyah"))
+eden.sim$pillar <- factor(eden.sim$pillar, levels = c("strong", "medium", "weak"))
+
+
+##########
+# SUM UP PLOTS
+##
+test.dice <- 8
+max.daat <- 7
+eden.plot <- ggplot(data=eden.sim %>% filter(dice.size == 8 & daat <= max.daat), aes(x = revelation)) + 
+  geom_bar(aes(y = (..count..)/tapply(..count..,..PANEL..,sum)[..PANEL..], fill=daat<3), colour="grey40", alpha=0.2) +
+  facet_grid(veil ~ pillar) +
+  labs(title=sprintf("Percentuale di successi (Da'at < %d, d%d)", max.daat + 1, test.dice)) +
+  scale_y_continuous(labels=percent) +
+  labs(x="Successi [Nr]", y="Risultati su tiri [%]")
+
+eden.plot
+
+eden.density <- ggplot(data=eden.sim %>% filter(dice.size == 8 & daat <= max.daat), aes(x = total.success)) + 
+  geom_density(aes(y = (..count..)/tapply(..count..,..PANEL..,sum)[..PANEL..]), color="grey40", fill="grey40", alpha=0.2, show.legend = FALSE) +
+  facet_grid(veil ~ pillar) +
+  labs(title=sprintf("Densità di successi (Da'at < %d, d%d)", max.daat + 1, test.dice)) +
+  labs(x="Successi [dens]", y="Risultati su tiri [%]")
+
+eden.density %+%
+  geom_density(data=eden.sim %>% filter(dice.size == 8 & daat <= max.daat & revelation > 0), aes(y = (..count..)/tapply(..count..,..PANEL..,sum)[..PANEL..]), , colour="red") %+%
+  geom_density(data=eden.sim %>% filter(dice.size == 8 & daat <= max.daat & sefiroth > 0), aes(y = (..count..)/tapply(..count..,..PANEL..,sum)[..PANEL..]), , colour="green") %+%
+  geom_density(data=eden.sim %>% filter(dice.size == 8 & daat <= max.daat & success > 0), aes(y = (..count..)/tapply(..count..,..PANEL..,sum)[..PANEL..]), , colour="blue")
---
+++ R/genesi.R
@@ -0,0 +1,112 @@
+genesi <- function(veil, pillar, malkuth, daat, dice.size){
+  
+  success <- c("asiyah" = dice.size - 1, "yetzirah" = dice.size - 2,"aziluth" = dice.size - 3)
+  pillars <- c("strong" = 1, "medium" = 0,"weak" = -1)
+  dice <- c("asiyah" = 4, "yetzirah" = 3, "aziluth" = 3)
+  
+  log.message <- "[%s] [d%d] *** Push a %s %s dice to %s using %d da'at dice"
+  
+  asiyah.throw <- as.integer(runif(ifelse(veil == "asiyah", dice["asiyah"] + pillars[pillar], ifelse(veil == "yetzirah", 1, 0)), 1, dice.size + 1))
+  yetzirah.throw <- as.integer(runif(ifelse(veil == "yetzirah", dice["yetzirah"] + pillars[pillar], 1), 1, dice.size + 1))
+  aziluth.throw <- as.integer(runif(ifelse(veil == "aziluth", dice["aziluth"] + pillars[pillar], ifelse(veil == "yetzirah", 1, 0)), 1, dice.size + 1))
+  
+  # The idea is to start from the nearest number to success and try to push it to nine using the lowest, then pass to others in
+  # decreasing order
+  
+  # Ignore daat on lateral pillars
+  if(pillar != "medium") daat <- 0
+  
+  throws.distance <- data.frame("veil" = factor(c(rep("asiyah", length(asiyah.throw)),
+                                                  rep("yetzirah", length(yetzirah.throw)),
+                                                  rep("aziluth", length(aziluth.throw))), level = c("asiyah", "yetzirah", "aziluth")),
+                                throw = c(asiyah.throw, yetzirah.throw, aziluth.throw),
+                                revelation.distance = c(success["asiyah"] - asiyah.throw + 2,
+                                                        success["yetzirah"] - yetzirah.throw + 2,
+                                                        success["aziluth"]  - aziluth.throw + 2),
+                                sefiroth.distance = c(success["asiyah"] - asiyah.throw + 1,
+                                                      success["yetzirah"]  - yetzirah.throw + 1,
+                                                      success["aziluth"] - aziluth.throw + 1),
+                                success.distance = c(success["asiyah"]  - asiyah.throw,
+                                                     success["yetzirah"] - yetzirah.throw,
+                                                     success["aziluth"] - aziluth.throw),
+                                stringsAsFactors = FALSE) %>% 
+    mutate(revelation.distance = ifelse(revelation.distance < 0, 0, revelation.distance)) %>%
+    mutate(sefiroth.distance = ifelse(sefiroth.distance < 0, 0, sefiroth.distance)) %>%
+    mutate(success.distance = ifelse(success.distance < 0, 0, success.distance)) %>%
+    filter(revelation.distance == 0 | sefiroth.distance == 0 | success.distance == 0 | throw <= daat) %>%
+    arrange(revelation.distance, sefiroth.distance, success.distance, desc(veil))
+
+  flog.debug(sprintf("[%s] [d%d] [daat=%d] [malkuth=%d] [%s] %s", veil, dice.size, daat, malkuth, pillar, paste("asiayah: ", paste(asiyah.throw, collapse = " "),
+                                                                                                                      "; yetzirah: ", paste(yetzirah.throw, collapse = " "),
+                                                                                                                      "; aziluth: ", paste(aziluth.throw, collapse = " "), sep = "")))
+  if(nrow(throws.distance) > 0){
+  
+  apply(throws.distance, 1, function(c.row){
+    
+    flog.trace(sprintf("[%s] [d%d] [type=%s] throw=%s\trevelation.distance=%s\tsefiroth.distance=%s\tsuccess.distance=%s",
+                       veil,
+                       dice.size,
+                       c.row['veil'],
+                       c.row['throw'],
+                       c.row['revelation.distance'],
+                       c.row['sefiroth.distance'],
+                       c.row['success.distance']))
+  })}
+
+  inc.success <- function(td){
+ 
+    if(nrow(td) == 0) return(NULL)
+    
+    throw.success <- data.frame()
+
+    if(!is.na(td[1, "revelation.distance"]) & td[1, "revelation.distance"] <= (nrow(td[2:nrow(td),] %>% filter(throw <= daat)))){
+      flog.debug(sprintf(log.message, veil, dice.size, pillar, td[1,"veil"], "revelation", td[1, "revelation.distance"], dice.size))
+      throw.success <- rbind(td[1,] %>%
+                               select("veil") %>%
+                               mutate(revelation = 1, sefiroth = 0, success = 0),
+                             inc.success((function(x){
+                               if(nrow(x) - x[1, "revelation.distance"] > 1) return(x[2:(nrow(x) - x[1, "revelation.distance"]),])
+                               else return(x[0,])
+                             })(td))
+      )
+    } else if(!is.na(td[1, "sefiroth.distance"]) & td[1, "sefiroth.distance"] <= (nrow(td[2:nrow(td),] %>% filter(throw <= daat)))){
+      flog.debug(sprintf(log.message, veil, dice.size, pillar, td[1,"veil"], "sefiroth", td[1, "sefiroth.distance"]))
+      throw.success <- rbind(td[1,] %>%
+                               select("veil") %>%
+                               mutate(revelation = 0, sefiroth = 1, success = 0),
+                             inc.success((function(x){
+                               if(nrow(x) - x[1, "sefiroth.distance"] > 1) return(x[2:(nrow(x) - x[1, "sefiroth.distance"]),])
+                               else return(x[0,])
+                             })(td))
+      )
+    } else if(!is.na(td[1, "success.distance"]) & td[1, "success.distance"] <= (nrow(td[2:nrow(td),] %>% filter(throw <= daat)))){
+      flog.debug(sprintf(log.message, veil, dice.size, pillar, td[1,"veil"], "success", td[1, "success.distance"]))
+      throw.success <- rbind(td[1,] %>%
+                               select("veil") %>%
+                               mutate(revelation = 0, sefiroth = 0, success = 1),
+                             inc.success((function(x){
+                               if(nrow(x) - x[1, "success.distance"] > 1) return(x[2:(nrow(x) - x[1, "success.distance"]),])
+                               else return(x[0,])
+                             })(td))
+      )
+    } else { 
+      
+      throw.success <- inc.success((function(x){
+        if(nrow(x) >= 2) return(x[2:nrow(x),])
+        else return(x[0,])
+      })(td))
+      
+      }
+   
+    return(throw.success)
+    
+  } 
+  
+  success.table <- inc.success(throws.distance) 
+  
+  if(is.null(success.table)) return(list("revelation" = 0, "sefiroth" = 0, "success" = 0))
+  
+  return(as.list(success.table %>% select(-veil) %>% summarize_all(sum)))
+  
+}
+
